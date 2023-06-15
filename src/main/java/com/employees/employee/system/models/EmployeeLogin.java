6809bf4e3abe4983f51e3bc311fded635dc38567
Wisdom=web247 <wizzymahlangu@gmail.com>
2023-06-15 23:23:15 +0200

    Created EmployeeModel for Login

---
+++ src/main/java/com/employees/employee/system/models/EmployeeLogin.java
@@ -0,0 +1,15 @@
+package com.employees.employee.system.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "EmployeeLogin")
+public class EmployeeLogin {
+    
+
+    
+}
