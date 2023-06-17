b452a99b868083bc4b24daf129437d03e94239bb
Wisdom=web247 <wizzymahlangu@gmail.com>
2023-06-18 00:17:26 +0200

    First Commit on Branch

--- src/main/java/com/employees/employee/system/models/EmployeeLogin.java
+++ src/main/java/com/employees/employee/system/models/EmployeeLogin.java
@@ -13,11 +13,12 @@ public class EmployeeLogin {
 @Min(3)
 private @NotBlank String userName;
 
+//Not sure why it created .java files in the Models Folder Automatically
 @Min(8)
 @Max(32)
 private @NotBlank String password;
 
-//Default Constructor
+//Default Constructor still not sure what it does
 public EmployeeLogin(){
 
 }
@@ -29,6 +30,21 @@ public EmployeeLogin(@NotBlank String username, @NotBlank String password){
 
 }
 
+//Getting UserName
+public String getUserName() {
+    return userName;
+}
 
-    
+public void setUserName(String userName) {
+    this.userName = userName;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
 }
